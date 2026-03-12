72d10d3209f17d1f0141d067922b1e7a7ba5aa7f
Rizhuycihuy <rizki003131@gmail.com>
2026-03-12 17:56:00 +0700

    add input

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -3,3 +3,27 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input()
+{
+      while (true){
+          cout << "masukkan jumlah data pada array : ";
+          cin >> n;
+
+          if (n <= 20){
+              break;
+          }
+          else{
+              cout << "\nArray tidak boleh lebih dari 20\n";
+          }
+      }
+      cout << endl;
+      cout << "=====================================";
+      cout << "=================massukkan elemen array============";
+      cout << "==========================================";
+
+      for (int i = 0; i < n; i++  ){
+          cout << "data ke-" << (i+1) << ": ";
+          cin >> arr [i];
+      }
+}
